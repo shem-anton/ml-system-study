d9d074003d8b69afbb9e79a952fd54f28590b323
Anton Shemyakov <shemyanton@gmail.com>
2021-09-13 13:24:13 +0200

    Switched to GUID as server id

--- api/app.py
+++ api/app.py
@@ -2,23 +2,24 @@ from flask import Flask
 import numpy as np
 import yfinance as yf
 from model import MeanModel
-import random
+import uuid
 
 
 app = Flask(__name__)
 model = MeanModel()
-id = random.random()
+id = str(uuid.uuid4())
 
 @app.route("/<ticker_id>")
 def serve_prediction(ticker_id):
     data = _fetch_historical_data(ticker_id)
     if data.shape[0] > 0:
         response = {
-            "prediction, id {}".format(id): model.predict(data)
+            "prediction": model.predict(data),
+            "server_id": id
         }
         return response
     else:
-        return "Ticker ID {} not available, id {}".format(ticker_id, id), 400
+        return "Ticker ID {} not available".format(ticker_id), 400
 
 def _fetch_historical_data(ticker_id):
     try:
