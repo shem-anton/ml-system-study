0eb4b7e427f2c78f20df4b293ca2f1f3d3631be4
Anton Shemyakov <anton@ai4medicine.com>
2021-11-03 17:49:41 +0100

    Debugging

--- ml_system_study/api/endpoints/predictions.py
+++ ml_system_study/api/endpoints/predictions.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException
 
 import configparser
 import uuid
-from asyncio import sleep
+from time import sleep
 from services.logging import LogService
 from services.prediction import PredictionService
 
@@ -19,9 +19,7 @@ logger.log("Assigned id {} to server".format(id))
 @router.get("/{ticker_id}")
 async def serve_prediction(ticker_id: str, model = "mean"):
     logger.log("API is queried for prediction on {}".format(ticker_id))
-    print("before")
-    sleep(1)
-    print("after")
+    sleep(2)
     prediction = prediction_service.predict(ticker_id, model)
     if prediction is None:
         raise HTTPException(status_code=400, detail=f"Ticker ID {ticker_id} not available")
